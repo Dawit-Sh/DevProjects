b813f209fc4f2a6ee0086ab1826748626bc373a2
nk <nk@localhost.localdomain>
2023-10-21 08:51:01 -0400

    update

--- Alpaca-Image-Generator/magic.js
+++ Alpaca-Image-Generator/magic.js
@@ -1,106 +1,53 @@
+// Title Settings 
+function mOut(obj) {
+ obj.innerHTML = "$~> Alpaca Generator" 
+} 
+function mOver(obj) {
+  obj.innerHTML = ":By Dawit-Sh"
+}
 console.log('Some Magic Is Happening 🪄')
 
-// vars 
-const eyesP = document.getElementById('eyes')
+const MainHair = document.querySelector("#Main_hair")
+const Maineyes = document.querySelector("#Main_eyes")
+const Mainear = document.querySelector("#Main_ear")
+const Mainleg = document.querySelector("#Main_leg")
+const Mainmouth = document.querySelector("#Main_mouth")
+const Mainneck = document.querySelector("#Main_neck")
+const Mainbg = document.querySelector("#Main_bg")
+const Mainacces = document.querySelector("#Main_acces")
 
+let div = document.getElementById('divbtn')
 
-
-// arrays for src 
-const hair = [
-    { text: "Default", src: "./alpaca/hair/default.png" },
-    { text: "Bang", src: "./alpaca/hair/bang.png" },
-    { text: "Curls", src: "./alpaca/hair/curls.png" },
-    { text: "Elegant", src: "./alpaca/hair/elegant.png" },
-    { text: "Fancy", src: "./alpaca/hair/fancy.png" },
-    { text: "Quiff", src: "./alpaca/hair/quiff.png" },
-    { text: "Short", src: "./alpaca/hair/short.png" }
-]
-
-const eyes = [
-    { text: "Default", src: "./alpaca/eyes/default.png" },
-    { text: "Angry", src: "./alpaca/eyes/angry.png" },
-    { text: "Naughty", src: "./alpaca/eyes/naughty.png" },
-    { text: "Panda", src: "./alpaca/eyes/panda.png" },
-    { text: "Smart", src: "./alpaca/eyes/smart.png" },
-    { text: "Star", src: "./alpaca/eyes/star.png" }
-]
-
-const ears = [
-    { text: "Default", src: "./alpaca/ears/default.png" },
-    { text: "Tilt Backward", src: "./alpaca/ears/tilt-backward.png" },
-    { text: "Tilt Forward", src: "./alpaca/ears/tilt-forward.png" }
-]
-
-const leg = [
-    { text: "Default", src: "./alpaca/leg/default.png" },
-    { text: "Bubble Tea", src: "./alpaca/leg/bubble-tea.png" },
-    { text: "Cookie", src: "./alpaca/leg/cookie.png" },
-    { text: "Game Console", src: "./alpaca/leg/game-console.png" },
-    { text: "Tilt Backward", src: "./alpaca/leg/tilt-backward.png" },
-    { text: "Tilt Forward", src: "./alpaca/leg/tilt-forward.png" }
-]
-
-const mouth = [
-    { text: "Default", src: "./alpaca/mouth/default.png" },
-    { text: "Astonished", src: "./alpaca/mouth/astonished.png" },
-    { text: "Eating", src: "./alpaca/mouth/eating.png" },
-    { text: "Laughing", src: "./alpaca/mouth/laugh.png" },
-    { text: "Tongue", src: "./alpaca/mouth/tongue.png" }
-]
-
-const neck = [
-    { text: "Default", src: "./alpaca/neck/default.png" },
-    { text: "Thick", src: "./alpaca/neck/thick.png" },
-    { text: "Bend Backward", src: "./alpaca/neck/bend-backward.png" },
-    { text: "Bend Forward", src: "./alpaca/neck/bend-forward.png" }
-]
-
-const accessories = [
-    { text: "None", src: "./alpaca/accessories/empty.png" },
-    { text: "Earings", src: "./alpaca/accessories/earings.png" },
-    { text: "Flower", src: "./alpaca/accessories/flower.png" },
-    { text: "Glasses", src: "./alpaca/accessories/glasses.png" },
-    { text: "Headphones", src: "./alpaca/accessories/headphone.png" }
-]
-
-const background = [
-    { name: "Blue 50", color: "#5596f6" },
-    { name: "Blue 60", color: "#2a7bf4" },
-    { name: "Blue 70", color: "#2a5ca7" },
-    { name: "Dark Blue 30", color: "#7a8faf" },
-    { name: "Dark Blue 50", color: "#002966" },
-    { name: "Dark Blue 70", color: "#00193d" },
-    { name: "Green 50", color: "#9fd966" },
-    { name: "Green 60", color: "#74bd29" },
-    { name: "Green 70", color: "#6a9440" },
-    { name: "Yellow 50", color: "#fcd33f" },
-    { name: "Yellow 60", color: "#e6b60d" },
-    { name: "Yellow 70", color: "#cf8e10" },
-    { name: "Red 50", color: "#e85e23" },
-    { name: "Red 60", color: "#cb4c15" },
-    { name: "Red 70", color: "#9f370a" },
-    { name: "Grey 40", color: "#cccccc" },
-    { name: "Grey 70", color: "#666666" },
-    { name: "Grey 80", color: "#333333" }
-]
-
-
-variable.map((item)=>{//do the changing logic here})
-
-
-    def
-  eyesP.map(eyes) {
+MainHair.addEventListener('click', () => {
+  // Remove the existing buttons.
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
   }
 
+  // Create new buttons.
+  let default = document.createElement('default');
+  button1.innerHTML = 'Default';
 
+  let bang = document.createElement('button');
+  button2.innerHTML = 'Bang';
 
+  let elegant = document.createElement('button');
+  button3.innerHTML = 'Elegant';
 
+  let short = document.createElement('button');
+  button4.innerHTML = 'Short';
 
-// Easter EGG   
-  document.getElementById("egg").addEventListener("click", easterEGG() {
-    alert("To Dough_Nut");
-  } );
+  let fancy = document.createElement('button');
+  button5.innerHTML = 'Fancy';
 
-  function Hidden() {
+  let quiff = document.createElement('button');
+  button6.innerHTML = 'Quiff';
 
-}
+  // Add the new buttons to the div element.
+  div.appendChild(default);
+  div.appendChild(bang);
+  div.appendChild(elegant);
+  div.appendChild(short);
+  div.appendChild(fancy);
+  div.appendChild(quiff);
+});
